d8bf44332f42f22ec4748c898a0ac87a2da44b47
0316d07cb3844f37b72d3883269fc780 <lizbeth.graf@gmail.com>
2021-03-18 21:51:00 +0100

    Setting things up ~

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((900, 400))
+
+running = True
+while running:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
