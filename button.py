9fbb12988e7c7958edf2b8300d56a31876d6dcff
0316d07cb3844f37b72d3883269fc780 <lizbeth.graf@gmail.com>
2021-04-11 13:40:17 +0200

    Button works a lot less badly

--- button.py
+++ button.py
@@ -1,8 +1,16 @@
 import pygame
 import pygame.font
 from pygame import mouse
+from enum import Enum
+
+class State(Enum):
+    NEUTRAL=0
+    HOVERED=1
+    PRESSED=2
 
 class Button (pygame.sprite.Sprite):
+
+
     def __init__(self, text, background_image, rect, on_click=[]):
         pygame.sprite.Sprite.__init__(self)
         # set up text
@@ -17,9 +25,33 @@ class Button (pygame.sprite.Sprite):
         self.on_click = on_click
 
         # internal state
+        self.state=State.NEUTRAL
 
     def update(self, *args, **kwargs) -> None:
-        if(mouse.get_pressed()[0]):
-            [todo() for todo in self.on_click]
+        Button.updater.get(self.state)(self)
+
+    def update_neutral(self):
+        if(self.rect.collidepoint(mouse.get_pos())):
+            self.state=State.HOVERED
+            self.update()
+
+    def update_hovered(self):
+        if(not self.rect.collidepoint(mouse.get_pos())):
+            self.state=State.NEUTRAL
+        else:
+            if (mouse.get_pressed()[0]):
+                [todo() for todo in self.on_click]
+                self.state=State.PRESSED
+    def update_pressed(self):
+        if (not mouse.get_pressed()[0]):
+            self.state = State.HOVERED
+
+
+    updater={
+        State.NEUTRAL:update_neutral,
+        State.HOVERED:update_hovered,
+        State.PRESSED:update_pressed
+    }
+
 
 
