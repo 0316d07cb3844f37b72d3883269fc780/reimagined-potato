07e34ece8853c2b5ff4f165f0d3f9ec6477432e4
0316d07cb3844f37b72d3883269fc780 <lizbeth.graf@gmail.com>
2021-12-21 00:58:02 +0100

    Fix tests

---
+++ game_data/test/test_game_state.py
@@ -0,0 +1,21 @@
+import unittest
+from game_data.src.game_state import Game_State as State
+from test_person_data import make_person
+
+def make_game_state():
+    allies,foes=[],[]
+
+    for _ in range(4):
+        ally, foe=make_person(),make_person()
+        allies.append(ally)
+        foes.append(foe)
+    return State(allies,foes)
+
+class MyTestCase(unittest.TestCase):
+    my_state = make_game_state()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
--- game_data/test/test_person_data.py
+++ game_data/test/test_person_data.py
@@ -4,14 +4,16 @@ from game_data.src.person_data import Person_Data as Person
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        my_person=Person(10, "Testtype")
+        my_person = self.make_person()
         my_person.damage(3)
         self.assertEqual(my_person.health,7,"Damage calculation error.")
 
 
 
 
-
-
 if __name__ == '__main__':
     unittest.main()
+
+def make_person():
+    my_person = Person(10, "Testtype")
+    return my_person
