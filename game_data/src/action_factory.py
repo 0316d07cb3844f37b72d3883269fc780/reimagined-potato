d46abe7992cd700a08434887df60974548e9c9f7
0316 <lizbeth.graf@gmail.com>
2022-10-05 00:02:47 +0200

    added some actionfactories

--- game_data/src/action_factory.py
+++ game_data/src/action_factory.py
@@ -2,12 +2,12 @@
 Attribute of a card responsible for generating actions when the card resolves.
 """
 
-from game_data.src.loadable import Loadable
+
 from game_data.src.action import *
 from enum import Enum
 
 
-class Action_Factory(Loadable):
+class Action_Factory:
     def __init__(self, factory_method, name : str):
         self.factory_method = factory_method
         self.factory_name = name
@@ -41,4 +41,11 @@ class Action_Factories(Action_Factory, Enum):
     tackle_factory = (create_tackle, "Tackle_Factory")
     brace_factory = (create_brace, "Brace_Factory")
     side_step_factory = (create_side_step, "Sidestep_Factory")
+    crushing_blow_factory = (create_crushing_blow, "Crushing_Blow_Factory")
+    tail_swipe_factory = (create_tail_swipe, "Tail_Swipe_Factory")
+    reckless_assault_factory = (create_reckless_assault, "Reckless_Assault_Factory")
+    bark_skin_blessing_factory = (create_bark_skin_blessing, "Bark_Skin_Blessing_Factory")
+    engulf_in_flames_factory = (create_engulf_in_flames, "Engulf_In_Flames_Factory")
+    sunshine_blessing_factory = (create_sunshine_blessing, "Sunshine_Blessing_Factory")
+    mind_blast_factory = (create_mind_blast, "Mind Blast Factory")
 
